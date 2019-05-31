03f81120ec03112b86eea20026176ef6c492b5af
andresAlas64 <andres.au95@gmail.com>
2019-05-31 15:50:26 -0600

    Cambios pequeños

--- include/navbar.php
+++ include/navbar.php
@@ -1,19 +1,19 @@
 <nav class="navbar navbar-expand-lg navbar-dark bg-color">
     <div class="container">
-        <a class="navbar-brand" href="#"><img src="img/codepen.png"></a>
+        <a class="navbar-brand" href="index.php" title="Inicio"><img src="img/codepen.png"></a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarNav">
             <ul class="navbar-nav ml-auto">
                 <li class="nav-item active">
-                    <a class="nav-link" href="index.php">Productos <span class="sr-only">(current)</span></a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="registrarse.php">Registrarse</a>
+                    <a class="nav-link" href="#">Productos <span class="sr-only">(current)</span></a>
                 </li>
                 <li class="nav-item">
                     <a class="nav-link" href="iniciarSesion.php">Iniciar sesión</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="registrarse.php">Registrarse</a>
                 </li>
                 <li class="nav-item">
                     <a class="nav-link" href="#"><i class="fas fa-shopping-cart"></i> Carrito</a>
